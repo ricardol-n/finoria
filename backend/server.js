8ed0e370fcd27b6d56530f453af0159be2fed890
eze levi <ezelevi7@gmail.com>
2026-06-19 07:38:27 +0100

    Configure server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: true,
+  origin: ["http://localhost:3000",
+    " https://finoria.onrender.com",
+  ],
   credentials: true,
 }));
 
